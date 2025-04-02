cad0a1b53af6e7633363458030f7ef310efaa91b
Rajat-Kaundal <kaundal.rajat18@gmail.com>
2025-04-02 12:00:29 +0530

    Changes to upload page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Upload from "./components/Upload";
 
 function App() {
   /*const [count, setCount] = useState(0) */
@@ -14,6 +15,7 @@ function App() {
       <header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Upload" element={<Upload />}></Route>
       </Routes>
     </Router>
   );
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -4,7 +4,7 @@ const Upload = () => {
     const [file, setFile] = useState(null);
 
     const handleFileChange = (e) => {
-        setFile(e.target.Files[0]);
+        setFile(e.target.files[0]);
     };
 
     const handleUpload = () => {
@@ -17,10 +17,9 @@ const Upload = () => {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-            <h1 className="text-3xl font-bold mb-4"> Upload your Resume</h1>
-            <input type="file" accept=".pdf, .doc, .docx" onChange={handleFileChange} className="mb-4" />
+            <h1 className="text-3xl font-bold mb-4">Upload your resume</h1>
+            <input type="file" accept=".pdf" onChange={handleFileChange} className="mb-4" />
             <button onClick={handleUpload} className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded"> Upload</button>
-
         </div>
     );
 };
